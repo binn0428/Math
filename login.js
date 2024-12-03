4d07d8c461f2e9fb30014904b9e1638f02351a67
binn0428 <156563833+binn0428@users.noreply.github.com>
2024-12-03 08:03:20 +0800

    1131203

--- login.js
+++ login.js
@@ -4,10 +4,10 @@ const CHECK_INTERVAL = 1000; // 每5秒檢查一次
 
 // 模擬用戶數據庫
 const defaultUsers = {
-    'binn0428': { password: 'pass0428', activeLogins: 0 },
-    'vic': { password: '1070320', activeLogins: 0 },
-    'yoyo': { password: '1031004', activeLogins: 0 },
-    'newuser': { password: 'newpass', activeLogins: 0 }
+    'binn0428': { password: '122232', activeLogins: 0, maxLogins: 1 },
+    'vic': { password: '1070320', activeLogins: 0, maxLogins: 1 },
+    'yoyo': { password: '1031004', activeLogins: 0, maxLogins: 1 },
+    'newuser': { password: 'newpass', activeLogins: 0, maxLogins: 1 }
 };
 
 // 初始化用戶數據
@@ -52,14 +52,14 @@ function login() {
     }
     
     if (users[username] && users[username].password === password) {
-        if (users[username].activeLogins === 0) {
-            users[username].activeLogins = 1;
+        if (users[username].activeLogins < users[username].maxLogins) {
+            users[username].activeLogins += 1;
             localStorage.setItem('users', JSON.stringify(users));
             sessionStorage.setItem('loginStatus', 'true');
             sessionStorage.setItem('username', username);
             window.location.href = 'index.html';
         } else {
-            showError('此帳號已在使用中');
+            showError('此帳號已在其他裝置登入中');
         }
     } else {
         showError('帳號或密碼錯誤');
